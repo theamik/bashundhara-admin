524332e1638dd5bba7acf7d3b84c0c74ef657506
theamik <amik.sylhet@gmail.com>
2023-11-21 22:40:21 +0600

    first commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,16 @@
-import axios from 'axios';
+import axios from 'axios'
+const  production = 'https://bashundhara-backend.onrender.com'
+const local = 'http://localhost:5000'
 
+let api_url = ''
+let mode =  "pro"
+if(mode === 'pro'){
+    api_url = production
+}else{
+    api_url = local
+}
 const api = axios.create({
-    baseURL: 'https://bashundhara-backend.onrender.com/api/v1'
-});
-
+    baseURL: `${api_url}/api/v1`,
+    withCredentials : true
+})
 export default api
